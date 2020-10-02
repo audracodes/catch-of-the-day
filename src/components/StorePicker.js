065888c5748a969475b1e92960cbae3cd19c5378
Audra T <audraturuta@gmail.com>
2020-10-02 15:59:48 -0400

    Starting Router, Work up to video 9

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,13 +3,11 @@ import React, {Fragment} from 'react';
 class StorePicker extends React.Component {
     render() {
         return (
-            <Fragment>
-                <form action="" className="store-selector">
-                    <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name"/>
-                    <button type="submit">Visit Store → </button>
-                </form>
-            </Fragment>
+            <form action="" className="store-selector">
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name"/>
+                <button type="submit">Visit Store → </button>
+            </form>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 // Can also say import {Component} from 'react'; 
 // and then it'd be class StorePicker extends Component
 import {render} from 'react-dom';
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+import Router from './components/Router';
 import './css/style.css'; 
 
 
-render(<App/>, document.querySelector('#main'));
+render(<Router/>, document.querySelector('#main'));
